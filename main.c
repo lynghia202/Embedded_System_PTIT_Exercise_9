0b4010ea3599599081b5eb3418139101da4dec1b
lynghia202 <136209431+lynghia202@users.noreply.github.com>
2025-09-28 18:10:44 +0700

    Update main.c

--- main.c
+++ main.c
@@ -45,25 +45,26 @@ static void ADC1_Init_CH0(void){
     gpio.GPIO_Mode = GPIO_Mode_AIN;
     GPIO_Init(GPIOA, &gpio);
 	
-    RCC_ADCCLKConfig(RCC_PCLK2_Div6);
-
+    RCC_ADCCLKConfig(RCC_PCLK2_Div6);//Cau hinh Clock cho ADC: Chia tan so Clock APB2 (PCLK2, thuong là 72MHz) cho 6.
+									// f_ADC: 72MHz / 6 = 12 MHz (Dam bao duoi gioi han 14MHz).
     ADC_InitTypeDef adc;
-    adc.ADC_Mode               = ADC_Mode_Independent; 
-    adc.ADC_ScanConvMode       = DISABLE;
-    adc.ADC_ContinuousConvMode = ENABLE;          
-    adc.ADC_ExternalTrigConv   = ADC_ExternalTrigConv_None;
-    adc.ADC_DataAlign          = ADC_DataAlign_Right;
-    adc.ADC_NbrOfChannel       = 1;
-    ADC_Init(ADC1, &adc);
+    adc.ADC_Mode               = ADC_Mode_Independent; // Che do hoat dong: ADC1 hoat dong doc lap (Khong ghep doi voi ADC khac).
+    adc.ADC_ScanConvMode       = DISABLE; // Che do Quet: Tat. Chi doc 1 kenh duy nhat (PA0).
+    adc.ADC_ContinuousConvMode = ENABLE;  // Che do Chuyen doi Lien tuc: Bat. ADC tu dong bat dau chuyen doi moi ngay sau khi xong.        
+    adc.ADC_ExternalTrigConv   = ADC_ExternalTrigConv_None; // Kich hoat ben ngoai: KHONG su dung su kien Timer hay chan ngoai de bat dau chuyen doi.
+    adc.ADC_DataAlign          = ADC_DataAlign_Right; // Canh chinh Du lieu: Gia tri RAW 12-bit duoc luu o ben phai (LSB) cua thanh ghi 16-bit.
+    adc.ADC_NbrOfChannel       = 1; // So luong Kenh: Dat la 1 vi ta chi doc kenh 0 (PA0).
+
+    ADC_Init(ADC1, &adc); // Khoi tao ADC1 voi cac tham so da cau hinh.
 
     ADC_Cmd(ADC1, ENABLE);
 		ADC_DMACmd(ADC1, ENABLE); //Enable DMA Request
 
     // Hieu chinh
-    ADC_ResetCalibration(ADC1);
-    while(ADC_GetResetCalibrationStatus(ADC1));
-    ADC_StartCalibration(ADC1);
-    while(ADC_GetCalibrationStatus(ADC1));
+    ADC_ResetCalibration(ADC1); // Reset lai gia tri hieu chinh cu.
+    while(ADC_GetResetCalibrationStatus(ADC1)); // Cho doi qua trinh Reset hieu chinh hoan tat.
+    ADC_StartCalibration(ADC1); // Bat dau qua trinh hieu chinh tu dong.
+    while(ADC_GetCalibrationStatus(ADC1)); // Cho doi qua trinh hieu chinh moi hoan tat (dam bao do chinh xac).
 		
 		ADC_SoftwareStartConvCmd(ADC1, ENABLE); 
 }
